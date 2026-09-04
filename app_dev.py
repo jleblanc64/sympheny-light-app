5a9329f98185e557375a803dc3a0d171be74aa22
jleblanc64 <dabadich@gmail.com>
2026-09-04 15:01:47 +0200

    improve cleanup commands

--- app_dev.py
+++ app_dev.py
@@ -16,4 +16,5 @@ os.environ[OS_JWT_OVERRIDE] = r.post(f"{base_url}backoffice/auth/ext/token",
                                      json={"email": username, "password": password}).json()["access_token"]
 
 subprocess.run(f"ps -eo pid,args | grep -E '[p]ython3? app.py' | awk '$1 != {os.getpid()} {{print $1}}' | xargs -r kill -9", shell=True)
+subprocess.run(f"ps -eo pid,args | grep -E '[p]ython3? app-dev.py' | awk '$1 != {os.getpid()} {{print $1}}' | xargs -r kill -9", shell=True)
 ipystream.run(use_xpython=False, show_logo=False, port=port, disable_extensions=True)
